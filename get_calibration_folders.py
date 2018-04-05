1f0e9f83a204533a623ca4ff2bb47566d37bc6f7
siivonek <kari.siivonen@tut.fi>
2018-04-05 11:14:03 +0300

    Add function to fetch calibration folders.

---
+++ get_calibration_folders.py
@@ -0,0 +1,35 @@
+import os
+
+def get_calibration_folders(root):
+    """
+    Returns calibration folders for each test subject inside root folder.
+    Also returns each calibration video folder inside calibration folder.
+    Returned values are stored in dictionary format.
+    """
+
+    directories = [dir for dir in os.listdir(root) if os.path.isdir(os.path.join(root, dir))]
+    print("Subjects found " + str(len(directories)))
+
+    output = {}
+
+    for subject in directories:
+
+        if os.path.isdir(os.path.join(root, subject)):
+            subject_dir = os.path.join(root, subject)
+            calib_dir = os.path.join(subject_dir, "calibrations")
+
+            # Debug
+            # print(subject)
+            # print(calib_dir)
+
+            calibrations = next(os.walk(calib_dir))[1]
+
+            # Debug
+            # print(calibrations)
+
+            output[subject] = calibrations
+
+    return output
+
+if __name__ == "__main__":
+    print(get_calibration_folders("C:\Local\siivonek\Data\eye_tracking_data\own_test_data\eyetrack_results"))
